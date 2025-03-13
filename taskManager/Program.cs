089e7e4484046fdc01ba6e3de25a6924479c1857
Alexis-Dpnt <dupssalex5@gmail.com>
2025-03-13 18:27:15 +0100

    updade 1

--- taskManager/Program.cs
+++ taskManager/Program.cs
@@ -1,4 +1,46 @@
-﻿// classe qui defini une tache
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+// class qui defini les parametres
+public class Parametre
+{
+    private String formatEnregistrement;
+    private String triDefaut;
+
+    public Parametre(String formatEnregistrement, String triDefaut)
+    {
+        this.formatEnregistrement = formatEnregistrement;
+        this.triDefaut = triDefaut;
+    }
+
+    // recuperer le format d'enregistrement
+    public String getFormatEnregistrement()
+    {
+        return this.formatEnregistrement;
+    }
+
+    // recuperer le mode de tri
+    public String getTriDefaut()
+    {
+        return this.triDefaut;
+    }
+
+    // redefinir le format d'enregistrement des taches
+    public void setFormatEnregistrement(String formatEnregistrement)
+    {
+        this.formatEnregistrement = formatEnregistrement;
+    }
+
+    // redefinir le mode de tri par defaut
+    public void setTriDefaut(String triDefaut)
+    {
+        this.triDefaut = triDefaut;
+    }
+}
+
+
+// classe qui defini une tache
 public class Tache
 {
     private String titre;
@@ -48,24 +90,28 @@ public class Tache
         return importance;
     }
 
+    // modifie le titre
     public void setTitre(String titre)
     {
         this.titre = titre;
     }
 
+    // modifie la description
     public void setDescription(String description)
     {
         this.description = description;
     }
 
+    // modifie l'importance
     public void setImportance(int importance)
     {
         this.importance = importance;
     }
 }
 
+
 // programme principal
-public class ex13
+public class Program
 {
     // fonction pour ajouter des taches
     public static Tache ajouterTache()
@@ -89,8 +135,10 @@ public class ex13
     {
         Console.Write("titre de la tache : ");
         taskList[index].affTitre();
+        
         Console.Write("description de la tache : ");
         taskList[index].affDescription();
+        
         Console.Write("importance de la tache : ");
         taskList[index].affImportance();
     }
@@ -98,11 +146,10 @@ public class ex13
     // fonction qui tri les taches par importance
     public static void triImportance(List<Tache> taskList)
     {
-        bool change;
+        bool change = true;
         int n = taskList.Count;
         Tache tacheTmp;
-
-        do
+        while (change)
         {
             change = false;
             for (int i = 0; i < n - 1; i++)
@@ -117,7 +164,6 @@ public class ex13
             }
             n--;
         }
-        while (change);
     }
     
     // fonction qui trie les taches par ordre alphabetique (choix de tri par defaut)
@@ -142,15 +188,43 @@ public class ex13
             n--;
         }
     }
+
+    public static Parametre recupParam()
+    {
+        string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "parametre.bin");
+
+        if (!File.Exists(filePath))
+        {
+            Console.WriteLine("Le fichier de paramètres n'existe pas.");
+            return null;
+        }
+
+        using (BinaryReader reader = new BinaryReader(File.Open(filePath, FileMode.Open)))
+        {
+            string tri = reader.ReadString();
+            string enregistrement = reader.ReadString();
+            return new Parametre(tri, enregistrement);
+        }
+    }
+    
+    public static void SauvegarderParametres(Parametre parametre)
+    {
+        string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "parametre.bin");
+        using (BinaryWriter writer = new BinaryWriter(File.Open(filePath, FileMode.Create)))
+        {
+            writer.Write(parametre.getFormatEnregistrement());
+            writer.Write(parametre.getTriDefaut());
+        }
+    }
     
     // fonction principale que je vais lancer dans le main dans Program.cs
     public static void Main(String[] args)
     {
+        Parametre parametre = recupParam();
         List<Tache> taskList = new List<Tache>();
-        // mettre variable triDefaut pour trier direct a l'ajout des taches
         int choix = 1;
         int numTache = 0;
-        while (choix != 7)
+        while (choix != 10)
         {
             Console.WriteLine("que veux tu faire ?");
             Console.WriteLine("1. ajouter une tache");
@@ -159,21 +233,24 @@ public class ex13
             Console.WriteLine("4. afficher une tache entierement");
             Console.WriteLine("5. modifier une tache");
             Console.WriteLine("6. trier la liste");
-            // ajouter sauvegarde des taches (regarde dans les parametres quel est le choix d'enregistrement) sauvegarde aussi les parametres
-            // ajouter charger une sauvegarde (detecte si binaire ou json)
-            // ajouter une option parametre qui vas gerer comment ca enregistre, quel est le tri par default ...
-            Console.WriteLine("9. quitter");
+            Console.WriteLine("7. sauvegarder une tache");
+            Console.WriteLine("9. parametres");
+            // les taches seront supprimées quand on quitte le programme et sont chargées quand le programme se lance
+            Console.WriteLine("10. quitter");
             choix = int.Parse(Console.ReadLine());
             switch (choix)
             {
-                // ajouter une tache
+                // option pour ajouter une tache
                 case 1:
                     Tache tacheAajouter = ajouterTache();
                     taskList.Add(tacheAajouter);
-                    // mettre fonction du triDefaut
+                    if (parametre.getTriDefaut().Equals("importance"))
+                        triImportance(taskList);
+                    else
+                        triAlpha(taskList);
                     break;
                 
-                // supprimer une tache
+                // option pour supprimer une tache
                 case 2:
                     Console.WriteLine("quel est le titre de la tache que tu veux supprimer ?\n");
                     String tacheAsupprimer = Console.ReadLine();
@@ -188,7 +265,7 @@ public class ex13
                     }
                     break;
                 
-                // voir toutes les taches
+                // option pour voir toutes les taches
                 case 3:
                     numTache = 0;
                     foreach (var taches in taskList)
@@ -198,7 +275,7 @@ public class ex13
                     }
                     break;
                 
-                // voir le detail d'une tache
+                // option pour voir le detail d'une tache
                 case 4:
                     Console.WriteLine("indique le numerot de la tache dont tu veux voir le detail");
                     int index = int.Parse(Console.ReadLine());
@@ -210,10 +287,11 @@ public class ex13
                     afficherTache(taskList, index);
                     break;
                 
-                // modifier une tache
+                // option pour modifier une tache
                 case 5:
                     Console.WriteLine("quel est le numero de la tache que tu veux modifier ?");
                     numTache = int.Parse(Console.ReadLine());
+                    
                     Console.WriteLine("veux tu modifier le titre de la tache ? (y/n)");
                     if (Console.ReadLine() == "y" || Console.ReadLine() == "Y")
                     {
@@ -221,7 +299,8 @@ public class ex13
                         String newTitre = Console.ReadLine();
                         taskList[numTache].setTitre(newTitre);
                     }
-                    if (Console.ReadLine() == "y" || Console.ReadLine() == "Y") ;
+                    if (Console.ReadLine() == "n" || Console.ReadLine() == "N") ;
+                    
                     Console.WriteLine("veux tu modifier la description de la tache ? (y/n)");
                     if (Console.ReadLine() == "y" || Console.ReadLine() == "Y")
                     {
@@ -230,6 +309,7 @@ public class ex13
                         taskList[numTache].setDescription(newDescription);
                     }
                     if (Console.ReadLine() == "n" || Console.ReadLine() == "N") ;
+                    
                     Console.WriteLine("veux tu modifier l'importance de la tache ? (y/n)");
                     if (Console.ReadLine() == "y" || Console.ReadLine() == "Y")
                     {
@@ -237,19 +317,18 @@ public class ex13
                         int newImportance = int.Parse(Console.ReadLine());
                         taskList[numTache].setImportance(newImportance);
                     }
-                    if (Console.ReadLine() == "y" || Console.ReadLine() == "Y") ;
                     break;
                 
-                // trier la liste des taches 
+                // option pour trier la liste des taches 
                 case 6:
                     Console.Clear();
-                    Console.WriteLine("comment veux tu trier tes taches");
-                    Console.WriteLine("1. par importance");
-                    Console.WriteLine("2. par ordre alphabetique (par defaut)");
-                    Console.WriteLine("3. retour");
                     int choixTri = 0;
                     while (choixTri != 3)
                     {
+                        Console.WriteLine("comment veux tu trier tes taches");
+                        Console.WriteLine("1. par importance");
+                        Console.WriteLine("2. par ordre alphabetique");
+                        Console.WriteLine("3. retour");
                         choixTri = int.Parse(Console.ReadLine());
     
                         switch (choixTri)
@@ -268,6 +347,50 @@ public class ex13
                         }
                     }
                     break;
+                
+                    // changer les parametres
+                    case 9:
+                        Console.Clear();
+                        int choixParam = 0;
+                        int choixTriP = 0;
+                        int choixEnregistrement = 0;
+                        while (choixParam != 3)
+                        {
+                            Console.WriteLine("que veux tu modifier ?");
+                            Console.WriteLine("1. format d'enregistrement : " + parametre.getFormatEnregistrement());
+                            Console.WriteLine("2. methode de tri : " + parametre.getTriDefaut());
+                            Console.WriteLine("3. quitter");
+                            choixParam = int.Parse(Console.ReadLine());
+                            if (choixParam == 1)
+                            {
+                                Console.WriteLine("quel format d'enregistrement veux tu utiliser ?");
+                                Console.WriteLine("1. bin");
+                                Console.WriteLine("2. Json");
+                                choixEnregistrement = int.Parse(Console.ReadLine());
+                                if (choixEnregistrement == 1)
+                                    parametre.setFormatEnregistrement("bin");
+                                else if (choixEnregistrement == 2)
+                                    parametre.setFormatEnregistrement("Json");
+                            }
+                            else if (choixParam == 2)
+                            {
+                                Console.WriteLine("quelle methode de tri veux tu utiliser ?");
+                                Console.WriteLine("1. tri par importance de tache");
+                                Console.WriteLine("2. tri des taches par ordre alphabetique");
+                                choixTriP = int.Parse(Console.ReadLine());
+                                if (choixTriP == 1)
+                                    parametre.setTriDefaut("importance");
+                                else if (choixTriP == 2)
+                                    parametre.setTriDefaut("alphabetique");
+                            }
+                            else if (choixParam == 3)
+                            {
+                                Console.WriteLine("format d'enregistrement : " + parametre.getFormatEnregistrement());
+                                Console.WriteLine("methode de tri : " + parametre.getTriDefaut());
+                                SauvegarderParametres(parametre);
+                            }
+                        }
+                        break;
             }
         }
     }
